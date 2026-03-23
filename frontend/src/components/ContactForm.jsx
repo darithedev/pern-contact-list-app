7a01de3605f62e77afa4e7352e1f18bc0d734317
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-23 17:11:29 -0400

    Add ContactForm component skeleton with useState, Form, and Button imports, contact useState and setter, submit handler and empty Form

---
+++ frontend/src/components/ContactForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+
+const ContactForm = ({ userId }) => {
+    const [contact, setContact] = useState({
+        user_id: userId,
+        name: "",
+        email: "",
+        phone_number: "",
+        address: "",
+        birthday: "",
+        notes: ""
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <Form 
+            className="form-contact"
+            onSubmit={handleSubmit}
+        >
+            
+        </Form>
+    )
+
+}
+
+export default ContactForm; 
