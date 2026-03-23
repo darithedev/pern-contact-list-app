31011773d5407b2ec9b1fe592cabccb266bb4a84
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-23 19:10:29 -0400

    Import auth and contacts routers, and /auth and /contacts routers

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import authRouter from './auth,js'
+import contactsRouter from './contacts.js'
 
 const router = express.Router();
 
-router.use('/', r);
+router.use('/auth', authRouter);
+router.use('/contacts', contactsRouter);
 
 export default router;
