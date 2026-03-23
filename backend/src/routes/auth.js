be0116395a2b9333bcf04ccefbc4595fee7bfe5e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-23 19:04:26 -0400

    Add auth.js routes skeleton

---
+++ backend/src/routes/auth.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import pool from '../db/pools.js'
+
+const router = express.Router();
+
+router.post('/login', async(req, res) => {
+
+});
+
+router.post('/signup', async(req, res) => {
+
+});
+
+router.get('/me', async(req, res) => {
+
+});
+
+router.put('/me', async(req, res) => {
+
+});
+
+router.delete('/me', async(req, res) => {
+
+});
