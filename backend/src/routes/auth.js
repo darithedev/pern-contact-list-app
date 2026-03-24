b07d446a0b88cd69a14a3bcbed6135c8b9ea3f4c
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-23 20:49:06 -0400

    Add completed login route where user is validated with password and issued a jwt token if authenticated sucessfully

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,9 +4,37 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 const router = express.Router();
+const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/login', async(req, res) => {
+    try {
+        const result = await pool.query(
+            `SELECT * FROM users WHERE email = $1`,
+            [email]
+        );
 
+        if (result.rows.length === 0) {
+            return res.status(400).json({
+                error: 'Error logging in to your account!'
+            });
+        }
+
+        const user = result.rows[0];
+        const loginResult = await bcrypt.compare(password, user.password);
+
+        if (!loginResult) {
+            return res.status(400).json({
+                error: 'Your email or password is invalid! Please try again.'
+            });
+        }
+
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET)
+
+        res.status(200).json({ token, user: { id: user.id, name: user.name, email: user.email } });
+    } catch (error) {
+        console.error('Error with login!', error);
+        res.status(500).json({ error: 'Error! Could not login user.' });
+    }
 });
 
 router.post('/signup', async(req, res) => {
