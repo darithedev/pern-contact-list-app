7fe9b094669480a47295f79a0fa0e10aa65f2dca
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-24 23:54:50 -0400

    Add get contact route to contact router

--- backend/src/routes/contacts.js
+++ backend/src/routes/contacts.js
@@ -4,7 +4,29 @@ import pool from '../db/pools.js'
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-
+    try {
+        const { id } = req.query;
+        const result = await pool.query(
+            //`SELECT * FROM contacts WHERE id = $1`,
+            `SELECT contacts.id, 
+                contacts.name, 
+                contacts.email, 
+                contacts.phone_number, 
+                contacts.address, 
+                contacts.birthday, 
+                contacts.notes, 
+                users.name 
+                AS owned_by
+            FROM contacts
+            JOIN users ON contacts.user_id = users.id
+            WHERE users.id = $1;`,
+            [id]
+        );
+        res.status(200).json(result.rows);
+    } catch(error) {
+        console.error('Error with getting contacts.', error);
+        res.status(500).json({ error: 'Error! Could not get contacts.' });
+    }
 });
 
 router.post('/', async(req, res) => {
