57d5e97eb3bd867ea43ec7422e5c40dba7e25250
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-27 22:05:33 -0400

    Remove ContactIndividual render and import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     <>
       <ContactForm />
       <ContactList />
-      <ContactIndividual />
     </>
   )
 }
--- frontend/src/components/ContactIndividual.jsx
+++ frontend/src/components/ContactIndividual.jsx
@@ -1,6 +1,21 @@
-const ContactIndividual = () => {
+import { useState, useEffect } from 'react'
+
+const ContactIndividual = ({ selectedContact, setSelectedContact, setEditing }) => {
+    const [contact, setContact] = useState("");
+
+    const handleEditContact = () => {
+        setEditing(false);
+    }
+
+    const goBack = () => {
+        setEditing(false);
+    }
+
     return (
         <>
+            {selectedContact}
+            <button onClick={() => handleEditContact(contact.id)}>submit</button>
+            <button onClick={() => goBack()}>back</button>
         </>
     )
 }
