f3fd10da741645ac99c80e017ee98e854501e1e2
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-27 22:07:16 -0400

    Add ContactList Component

--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react'
+import ContactIndividual from './ContactIndividual';
+
 const ContactList = () => {
+    const [contactList, setContactList] = useState([]);
+    const [editing, setEditing] = useState(false);
+    const [selectedContact, setSelectedContact] = useState("");
+
+    const getContacts = () => {
+        fetch("http://localhost:8080/api/contacts", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        })
+        .then((response) => response.json())
+        .then((contacts) => {
+            setContactList(contacts);
+        });
+    };
+
+    const handleEditContact = (contactId) => {
+        setEditing(true);
+        setSelectedContact(contactId);
+    }
+
+    useEffect(() => {
+        getContacts();
+    }, []);
+    
     return (
         <>
+            {!editing ? (
+                contactList.map((contact) => (
+                    <div key={contact.id}>
+                        <button onClick={() => handleEditContact(contact.id)}>Edit</button>
+                        <li>{contact.name}</li>
+                    </div>
+                ))
+            ) : (
+                <ContactIndividual 
+                    selectedContact={selectedContact}
+                    setSelectedContact={setSelectedContact}
+                    setEditing={setEditing}
+                />
+            )}
         </>
     )
 }
