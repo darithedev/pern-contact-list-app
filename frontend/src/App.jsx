f52e7465c77160b01eeff03874df13556f62b61a
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-27 20:45:33 -0400

    Import ContactList and render ContactList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import ContactForm from './components/ContactForm'
+import ContactList from './components/ContactList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <ContactForm />
+      <ContactList />
     </>
   )
 }
