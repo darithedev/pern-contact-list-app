c633f549fd0042658b84c57d56188bb4f9a02f8e
Dari Caceres <80991896+darithedev@users.noreply.github.com>
2026-03-27 22:06:19 -0400

    Remove ContactIndividual import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import ContactForm from './components/ContactForm'
 import ContactList from './components/ContactList'
-import ContactIndividual from './components/ContactIndividual'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
